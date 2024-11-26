127bf3ba80c098bdefeb9e29b8e444e5f149b3b4
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2024-11-26 14:24:00 +0700

    update model
    add model and migration for ads banner, article, author, category

---
+++ app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'content',
+        'category_id',
+        'author_id',
+        'is_featured'
+    ];
+}
