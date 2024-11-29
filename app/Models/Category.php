ef4653c145a0318a7eb62ef669e522b44fa12d71
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2024-11-29 15:12:29 +0700

    make filament resources for category model
    make create, read, update, delete for data category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -17,6 +18,10 @@ class Category extends Model
         'icon'
     ] ;
 
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
     public function article(): HasMany{
         return $this->hasMany(Article::class);
     }
