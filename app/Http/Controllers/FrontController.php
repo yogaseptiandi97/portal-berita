96f62a4ce3533a0561c6d497644309a26434cffd
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2024-12-06 12:03:40 +0700

    update blade index
    update category, featured articles, latest articles

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\Author;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -10,6 +12,22 @@ class FrontController extends Controller
     //
     public function index(){
         $categories = Category::all();
-        return view('front.index',compact('categories'));
+
+        $articles = Article::with(['category'])
+        ->where('is_featured', 2)
+        ->latest()
+        ->take(3)
+        ->get();
+
+        $featured = Article::with(['category'])
+        ->where('is_featured', 1)
+        ->latest()
+        ->take(3)
+        ->get();
+
+        $authors = Author::all();
+
+
+        return view('front.index',compact('categories', 'articles', 'featured'));
     }
 }
