8d1a290ce295603b77c3fa30bc2074bc09722a34
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2024-12-09 22:25:18 +0700

    make a template page
    rapikan codingan / don't repeat yourself

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -1,78 +1,68 @@
-<!DOCTYPE html>
-<html>
+@extends('front.template')
+@section('content')
 
-<head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link href="{{ asset('output.css') }}" rel="stylesheet" />
-    <link href="{{ asset('main.css') }}" rel="stylesheet" />
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
-		rel="stylesheet" />
-        <script src="https://cdn.tailwindcss.com"></script>
-</head>
+    <body class="font-[Poppins] pb-[83px]">
+        <x-navbar />
+        <nav id="Category" class="max-w-[1130px] mx-auto flex justify-center items-center gap-4 mt-[30px]">
+            @foreach ($categories as $items_category)
+                <a href="{{ route('front.category', $items_category->slug) }}"
+                    class="rounded-full p-[12px_22px] flex gap-[10px] font-semibold transition-all duration-300 border border-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18]">
+                    <div class="w-6 h-6 flex shrink-0">
+                        <img src="{{ Storage::url($items_category->icon) }}" alt="icon" />
+                    </div>
+                    <span>{{ $items_category->name }}</span>
+                </a>
+            @endforeach
 
-<body class="font-[Poppins] pb-[83px]">
-	<x-navbar/>
-	<nav id="Category" class="max-w-[1130px] mx-auto flex justify-center items-center gap-4 mt-[30px]">
-        @foreach ($categories as $items_category)
-            <a href="{{ route('front.category', $items_category->slug) }}"
-                class="rounded-full p-[12px_22px] flex gap-[10px] font-semibold transition-all duration-300 border border-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18]">
-                <div class="w-6 h-6 flex shrink-0">
-                    <img src="{{ Storage::url($items_category->icon) }}" alt="icon" />
-                </div>
-                <span>{{ $items_category->name }}</span>
-            </a>
-        @endforeach
-
-    </nav>
-	<section id="Category-result" class="max-w-[1130px] mx-auto flex items-center flex-col gap-[30px] mt-[70px]">
-		<h1 class="text-4xl leading-[45px] font-bold text-center">
-			Explore Our <br />
-			{{ $category->name }} News
-		</h1>
-		<div id="search-cards" class="grid grid-cols-1 gap-[30px] sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-[30px] ">
-            @forelse ($category->article as $article)
-                
-			<a href="{{ route('front.details', $article->slug) }}" class="card">
-				<div
-					class="flex flex-col gap-4 p-[26px_20px] transition-all duration-300 ring-1 ring-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18] rounded-[20px] overflow-hidden bg-white">
-					<div class="thumbnail-container h-[200px] relative rounded-[20px] overflow-hidden">
-						<div
-							class="badge absolute left-5 top-5 bottom-auto right-auto flex p-[8px_18px] bg-white rounded-[50px]">
-							<p class="text-xs leading-[18px] font-bold uppercase">{{ $article->category->name }}</p>
-						</div>
-						<img src="{{ Storage::url($article->thumbnail) }}" alt="thumbnail photo"
-							class="w-full h-full object-cover" />
-					</div>
-					<div class="flex flex-col gap-[6px]">
-						<h3 class="text-lg leading-[27px] font-bold">{{ substr($article->name, 0, 50) }}
-                            {{ strlen($article->name) > 50 ? '...' : '' }}</h3>
-						<p class="text-sm leading-[21px] text-[#A3A6AE]">{{ $article->created_at->format('M d, Y') }}</p>
-					</div>
-				</div>
-			</a>
-            @empty
-            <div class="flex flex-col text-center gap-[14px] items-center">
-                <h2 class="font-bold text-[16px] leading-[39px]">
-                    Belum ada berita terbaru
-                </h2>
+        </nav>
+        <section id="Category-result" class="max-w-[1130px] mx-auto flex items-center flex-col gap-[30px] mt-[70px]">
+            <h1 class="text-4xl leading-[45px] font-bold text-center">
+                Explore Our <br />
+                {{ $category->name }} News
+            </h1>
+            <div id="search-cards"
+                class="grid grid-cols-1 gap-[30px] sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-[30px] ">
+                @forelse ($category->article as $article)
+                    <a href="{{ route('front.details', $article->slug) }}" class="card">
+                        <div
+                            class="flex flex-col gap-4 p-[26px_20px] transition-all duration-300 ring-1 ring-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18] rounded-[20px] overflow-hidden bg-white">
+                            <div class="thumbnail-container h-[200px] relative rounded-[20px] overflow-hidden">
+                                <div
+                                    class="badge absolute left-5 top-5 bottom-auto right-auto flex p-[8px_18px] bg-white rounded-[50px]">
+                                    <p class="text-xs leading-[18px] font-bold uppercase">{{ $article->category->name }}</p>
+                                </div>
+                                <img src="{{ Storage::url($article->thumbnail) }}" alt="thumbnail photo"
+                                    class="w-full h-full object-cover" />
+                            </div>
+                            <div class="flex flex-col gap-[6px]">
+                                <h3 class="text-lg leading-[27px] font-bold">{{ substr($article->name, 0, 50) }}
+                                    {{ strlen($article->name) > 50 ? '...' : '' }}</h3>
+                                <p class="text-sm leading-[21px] text-[#A3A6AE]">
+                                    {{ $article->created_at->format('M d, Y') }}</p>
+                            </div>
+                        </div>
+                    </a>
+                @empty
+                    <div class="flex flex-col text-center gap-[14px] items-center">
+                        <h2 class="font-bold text-[16px] leading-[39px]">
+                            Belum ada berita terbaru
+                        </h2>
+                    </div>
+                @endforelse
             </div>
-            @endforelse
-		</div>
-	</section>
-	<section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
-		<div class="flex flex-col gap-3 shrink-0 w-fit">
-			<a href="{{ $ads->link }}">
-				<div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
-					<img src="{{ Storage::url($ads->thumbnail) }}" class="object-cover w-full h-full" alt="ads" />
-				</div>
-			</a>
-			<p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
-				Our Advertisement <a href="#" class="w-[18px] h-[18px]"><img
-						src="{{asset('assets/images/icons/message-question.svg')}}" alt="icon" /></a>
-			</p>
-		</div>
-	</section>
-</body>
-
-</html>
+        </section>
+        <section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
+            <div class="flex flex-col gap-3 shrink-0 w-fit">
+                <a href="{{ $ads->link }}">
+                    <div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
+                        <img src="{{ Storage::url($ads->thumbnail) }}" class="object-cover w-full h-full" alt="ads" />
+                    </div>
+                </a>
+                <p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
+                    Our Advertisement <a href="#" class="w-[18px] h-[18px]"><img
+                            src="{{ asset('assets/images/icons/message-question.svg') }}" alt="icon" /></a>
+                </p>
+            </div>
+        </section>
+    </body>
+@endsection
