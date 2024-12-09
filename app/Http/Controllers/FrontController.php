e675b944b45b0576bf0cf8fd78b74a4103605322
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2024-12-09 22:10:14 +0700

    add search function
    make search function and page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -78,11 +78,26 @@ class FrontController extends Controller
 
     public function author(Author $author){
         $categories = Category::all();
-        
+
         $ads = AdsBanner::where('is_active', 'active')
         ->where('type', 'banner')
         ->inRandomOrder()
         ->first();
         return view('front.author', compact('categories', 'author','ads'));
+    }
+
+    public function search(Request $request){
+        $request->validate([
+            'keyword' => ['required', 'string', 'max:255'],
+        ]);
+
+        $categories = Category::all();
+
+        $keyword = $request->keyword;
+
+        $articles = Article::with(['category', 'author'])
+        ->where('name', 'like', '%'.$keyword.'%')->paginate(6);
+
+        return view('disfrontplay.search', compact('articles', 'keyword', 'categories'));
     }
 }
